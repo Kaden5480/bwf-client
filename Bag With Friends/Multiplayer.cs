38fc3e1e24e6f9566611def133ea1f743a54fbc1
Givo <giv0online@gmail.com>
2024-03-03 20:05:08 -0600

    new updater

--- Bag With Friends/Multiplayer.cs
+++ Bag With Friends/Multiplayer.cs
@@ -326,47 +326,6 @@ namespace Bag_With_Friends
                     LoggerInstance.Msg("Custom server");
                 }
             }
-
-            using (var client = new WebClient())
-            {
-                await client.DownloadFileTaskAsync(
-                    new System.Uri("https://bwf.givo.xyz/Bag_With_Friends/Mods/Bag%20With%20Friends.dll"),
-                    "./Bag With Friends.dll"
-                );
-
-                if (!FileCompare("./Mods/Bag With Friends.dll", "./Bag With Friends.dll") && !debugMode)
-                {
-                    List<string> launchArgs = new List<string>(0);
-                    launchArgs.AddRange(Environment.GetCommandLineArgs());
-                    launchArgs.RemoveAt(0);
-
-                    File.WriteAllText("./LastStartInfo.txt", String.Join(" ", launchArgs.ToArray()));
-
-                    System.Reflection.Assembly asm = MelonAssembly.Assembly;
-                    Stream updateScriptRead = asm.GetManifestResourceStream("Bag_With_Friends.UpdateBWF.ps1");
-                    FileStream updateScriptWrite = new FileStream("./UpdateBWF.ps1", FileMode.OpenOrCreate, FileAccess.Write);
-                    updateScriptRead.CopyTo(updateScriptWrite);
-                    updateScriptRead.Close();
-                    updateScriptWrite.Close();
-
-                    ProcessStartInfo processInfo;
-                    Process process;
-                    processInfo = new ProcessStartInfo("powershell.exe", "-File .\\UpdateBWF.ps1");
-                    processInfo.CreateNoWindow = false;
-                    processInfo.UseShellExecute = true;
-
-                    process = Process.Start(processInfo);
-
-                    await Task.Delay(5000);
-
-                    Application.Quit();
-                } else
-                {
-                    File.Delete("./Bag With Friends.dll");
-                    File.Delete("./LastStartInfo.txt");
-                    File.Delete("./UpdateBWF.ps1");
-                }
-            }
         }
 
         IEnumerator MakeShadowPrefab()
@@ -1492,57 +1451,6 @@ namespace Bag_With_Friends
             }
 
             return components.ToArray();
-        }
-
-        private bool FileCompare(string file1, string file2)
-        {
-            int file1byte;
-            int file2byte;
-            FileStream fs1;
-            FileStream fs2;
-
-            // Determine if the same file was referenced two times.
-            if (file1 == file2)
-            {
-                // Return true to indicate that the files are the same.
-                return true;
-            }
-
-            // Open the two files.
-            fs1 = new FileStream(file1, FileMode.Open, FileAccess.Read);
-            fs2 = new FileStream(file2, FileMode.Open, FileAccess.Read);
-
-            // Check the file sizes. If they are not the same, the files 
-            // are not the same.
-            if (fs1.Length != fs2.Length)
-            {
-                // Close the file
-                fs1.Close();
-                fs2.Close();
-
-                // Return false to indicate files are different
-                return false;
-            }
-
-            // Read and compare a byte from each file until either a
-            // non-matching set of bytes is found or until the end of
-            // file1 is reached.
-            do
-            {
-                // Read one byte from each file.
-                file1byte = fs1.ReadByte();
-                file2byte = fs2.ReadByte();
-            }
-            while ((file1byte == file2byte) && (file1byte != -1));
-
-            // Close the files.
-            fs1.Close();
-            fs2.Close();
-
-            // Return the success of the comparison. "file1byte" is 
-            // equal to "file2byte" at this point only if the files are 
-            // the same.
-            return ((file1byte - file2byte) == 0);
         }
     }
 }
