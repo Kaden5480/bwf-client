b14b35fc094fc25b2bf41961d8b89dedabc304f3
Kaden5480 <kaden5480@protonmail.com>
2024-04-15 00:29:25 +0100

    Add option for encryption

--- src/Preferences.cs
+++ src/Preferences.cs
@@ -2,19 +2,33 @@ using MelonLoader;
 
 namespace Bag_With_Friends {
     public class Preferences {
+        private string filePath = "UserData/BagWithFriends.cfg";
+
         private MelonPreferences_Category server;
         public MelonPreferences_Entry<string> host { get; }
         public MelonPreferences_Entry<int> port    { get; }
 
+        private MelonPreferences_Category dev;
+        public MelonPreferences_Entry<bool> encrypted { get; }
+
         public Preferences() {
             this.server = MelonPreferences.CreateCategory("server");
-            this.server.SetFilePath("UserData/BagWithFriends.cfg");
+            this.server.SetFilePath(this.filePath);
 
             this.host = this.server.CreateEntry<string>("host", "bwf.givo.xyz");
             this.port = this.server.CreateEntry<int>("port", 3000);
+
+            this.dev = MelonPreferences.CreateCategory("dev");
+            this.dev.SetFilePath(this.filePath);
+
+            this.encrypted = this.dev.CreateEntry<bool>("encrypted", true);
         }
 
         public string GetServer() {
+            if (this.encrypted.Value == true) {
+                return $"wss://{this.host.Value}:{this.port.Value}";
+            }
+
             return $"ws://{this.host.Value}:{this.port.Value}";
         }
     }
